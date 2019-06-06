35b638d678ffa6652573ccc3d2009cffd66626a9
Jan <13890391+Urtgard@users.noreply.github.com>
2019-06-06 11:03:22 +0200

    singleton config

---
+++ Java/MetroMaps/src/Config.java
@@ -0,0 +1,16 @@
+
+public class Config {
+	private static Config instance;
+	
+	String name;
+
+	private Config() {
+	}
+
+	public static Config getInstance() {
+		if (Config.instance == null) {
+			Config.instance = new Config();
+		}
+		return Config.instance;
+	}
+}
--- Java/MetroMaps/src/Core.java
+++ Java/MetroMaps/src/Core.java
@@ -1,12 +1,13 @@
 
 public class Core {
-	public static String graphName = "sydney";
-
 	public static void main(String[] args) {
+		Config config = Config.getInstance();
+		config.name = "wien";
+		
 		XMLParser parser = new XMLParser();
 		// MetroMap map = parser.getMapFromXML("berlin.xml");
 		Core c = new Core();
-		MetroMap map = parser.getMapFromXML(graphName + ".graphml");
+		MetroMap map = parser.getMapFromXML(config.name + ".graphml");
 		// System.out.println(map);
 		Solver s = new Solver();
 		s.solve(map);
